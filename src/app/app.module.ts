4a0c4edb0d6d1db75cbeb1bb534c524848b6939a
Jim Hessin <jhessin@gmail.com>
2021-07-26 12:25:35 -0600

    gave up on yarn 2... again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  imports: [BrowserModule],
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgbModule, FontAwesomeModule],
 })
 export class AppModule {}
